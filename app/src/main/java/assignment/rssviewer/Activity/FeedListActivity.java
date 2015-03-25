a19a23cd2388a361ec689b0e5801f7b20d44aed5
khanhhuy <khanhhuy.dev@gmail.com>
2015-03-25 21:37:46 +0700

    Add Drawer + Action Bar

--- app/src/main/java/assignment/rssviewer/Activity/FeedListActivity.java
+++ app/src/main/java/assignment/rssviewer/Activity/FeedListActivity.java
@@ -1,22 +1,41 @@
 package assignment.rssviewer.activity;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import assignment.rssviewer.R;
 
 
 public class FeedListActivity extends ActionBarActivity {
 
+    private DrawerLayout mDrawer;
+    private ListView mDrawerList;
+    private String[] mDrawerTitles;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_feed_list);
+        setContentView(R.layout.feedlist_layout);
+
+
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerTitles = getResources().getStringArray(R.array.drawer_list);
+
+        mDrawerList.setAdapter(new ArrayAdapter<String>(
+                this, R.layout.draweritem_layout, mDrawerTitles
+        ));
+
     }
 
     //somecomment
+    //Dropdown in drawer
+    //http://stackoverflow.com/questions/23195740/how-to-implement-android-navigation-drawer-like-this#
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
