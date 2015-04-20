77ca57508d0c090714c56b9c4ed1f0556574afef
khanhhuy <khanhhuy.dev@gmail.com>
2015-04-20 19:56:25 +0700

    Get category list from Database

--- app/src/main/java/assignment/rssviewer/Activity/main/FeedListFragment.java
+++ app/src/main/java/assignment/rssviewer/Activity/main/FeedListFragment.java
@@ -17,22 +17,24 @@ import assignment.rssviewer.R;
 import assignment.rssviewer.activity.WebViewActivity;
 import assignment.rssviewer.adapter.PostListAdapter;
 import assignment.rssviewer.model.Article;
+import assignment.rssviewer.model.Category;
 import assignment.rssviewer.model.RssSource;
+import assignment.rssviewer.service.IDataService;
+import assignment.rssviewer.service.RssApplication;
 import assignment.rssviewer.service.RssParser;
+import assignment.rssviewer.utils.Action;
+import assignment.rssviewer.utils.AsyncResult;
 import assignment.rssviewer.utils.ListViewHelper;
 
-public class FeedListFragment extends BaseMainFragment
-{
+public class FeedListFragment extends BaseMainFragment {
     private List<Article> listArticle;
     private PostListAdapter adapter;
     private ListViewHelper.SupportWidget supportWidget;
 
-    private AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener()
-    {
+    private AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
 
         @Override
-        public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3)
-        {
+        public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
             Article article = listArticle.get(arg2);
             Bundle articLink = new Bundle();
             articLink.putString("url", article.getUrlString());
@@ -43,21 +45,18 @@ public class FeedListFragment extends BaseMainFragment
         }
     };
 
-    public FeedListFragment()
-    {
+    public FeedListFragment() {
         this.setTitle("Feed List");
         this.setIconResource(R.drawable.ic_action_expand);
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
-    {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         Activity activity = getActivity();
         View view = inflater.inflate(R.layout.fragment_feed_list, container, false);
         listArticle = new ArrayList<>();
@@ -77,54 +76,48 @@ public class FeedListFragment extends BaseMainFragment
         final List<RssSource> rssSourceList = new ArrayList<>();
         rssSourceList.add(new RssSource(0L, "Gamespot", "http://www.gamespot.com/feeds/news/", 0));
         rssSourceList.add(new RssSource(0L, "Gamespot", "http://www.theverge.com/rss/index.xml", 0));
-        new AsyncTask<String, Integer, List<Article>>()
-        {
+        new AsyncTask<String, Integer, List<Article>>() {
             @Override
-            protected void onPreExecute()
-            {
+            protected void onPreExecute() {
                 supportWidget.toggleStatus(ListViewHelper.Status.LOADING);
             }
 
             @Override
-            protected List<Article> doInBackground(String... params)
-            {
+            protected List<Article> doInBackground(String... params) {
                 RssParser parser = new RssParser();
                 return parser.parseArticles(rssSourceList);
             }
 
             @Override
-            protected void onPostExecute(List<Article> result)
-            {
+            protected void onPostExecute(List<Article> result) {
                 Log.d("Article List: ", Integer.valueOf(result.size()).toString());
                 supportWidget.toggleStatus(ListViewHelper.Status.NORMAL);
-                for (Article article : result)
-                {
+                for (Article article : result) {
                     listArticle.add(article);
                 }
                 adapter.notifyDataSetChanged();
             }
         }.execute();
 
+        getListCategory();
+
         return view;
     }
 
     @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
-    {
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.menu_feed_list, menu);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
+    public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
-        {
+        if (id == R.id.action_settings) {
             return true;
         }
 
@@ -132,25 +125,44 @@ public class FeedListFragment extends BaseMainFragment
     }
 
     @Override
-    public boolean isStatic()
-    {
+    public boolean isStatic() {
         return true;
     }
 
 
-    /*private void generateDummyData(List<PostData> listData)
-    {
-        listData.clear();
+    private IDataService dataService;
 
-        PostData data;
-        for (int i = 0; i < 20; i++)
-        {
-            data = new PostData();
-            data.postDate = "April 2, 2015";
-            data.postTitle = "Post " + (i + 1)
-                    + " Title: This is the Post Title from RSS Feed";
-            data.postThumbUrl = null;
-            listData.add(data);
-        }
-    }*/
+    private List<Category> getListCategory() {
+
+        final Activity activity = getActivity();
+        final RssApplication application = (RssApplication) activity.getApplication();
+        dataService = application.getDataService();
+
+        final List<Category> categoryList = new ArrayList<>();
+
+        dataService.initializeAsync(new Action<AsyncResult<Void>>() {
+            @Override
+            public void execute(final AsyncResult<Void> initResult) {
+                if (initResult.isSuccessful()) {
+                    dataService.loadAllAsync(Category.class, new Action<AsyncResult<List<Category>>>() {
+                        @Override
+                        public void execute(AsyncResult<List<Category>> loadResult) {
+                            if (loadResult.isSuccessful()) {
+                                ArrayList<Category> catList = new ArrayList<Category>(loadResult.getResult());
+                                for (Category category : catList) {
+                                    Log.d("Category", category.getName());
+                                    if (category.getRssSources().size() > 0)
+                                        Log.d("Category Rss", category.getRssSources().get(0).getName());
+                                    categoryList.add(category);
+                                }
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        return categoryList;
+    }
+
 }
